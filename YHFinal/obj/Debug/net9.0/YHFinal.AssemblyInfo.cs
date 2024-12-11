e45b4197a57d5cbd4474542338f0a5b7e799ffa4
Cloudiiiii <91622378+Cloudiiiii@users.noreply.github.com>
2024-12-11 02:41:48 -0500

    init
    init

--- YHFinal/obj/Debug/net9.0/YHFinal.AssemblyInfo.cs
+++ YHFinal/obj/Debug/net9.0/YHFinal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YHFinal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0c4e1db088ddc247cd49422dd1e3e81cc6ca4f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+911c038919017b70e29d57d274faf65b9eb9cd68")]
 [assembly: System.Reflection.AssemblyProductAttribute("YHFinal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YHFinal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
